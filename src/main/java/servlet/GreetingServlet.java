b19846deff7c5f8b8f9a1e53b58bea52ac2cfdf1
SavvaMey <savva28031997@gmail.com>
2021-03-02 13:17:33 +0300

    4.1. JSON формат. [#474341]

--- src/main/java/servlet/GreetingServlet.java
+++ src/main/java/servlet/GreetingServlet.java
@@ -1,22 +1,30 @@
 package servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.setContentType("text/plain");
+        resp.setContentType("json");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        System.out.println( req.getReader().lines().collect(Collectors.joining()));
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
+        JSONObject json = new JSONObject();
+        json.put("email", req.getParameter("text"));
+        writer.println(json);
         writer.flush();
     }
 }
